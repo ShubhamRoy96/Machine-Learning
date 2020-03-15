6d7089748caebb268d82123dd2ccdfdf6e1388d6
ShubhamRoy96 <shubhamroy896@gmail.com>
2020-03-15 22:10:48 +0530

    Multiple Linear regression using startup data

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/50Stratups.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/50Stratups.R
@@ -1,1 +1,9 @@
 startup_data = read.csv('50_Startups.csv')
+startup_data$State = factor(x = startup_data$State, levels = c('New York', 'California', 'Florida'), labels = c(1, 2, 3))
+splitset = sample.split(Y = startup_data$Profit,SplitRatio = 0.8)
+training_Set = subset(x = startup_data, splitset == TRUE)
+test_Set = subset(x = startup_data, splitset == FALSE)
+regressor = lm(formula = Profit ~ .,data = training_Set)
+
+
+y_pred = predict(regressor,newdata = test_Set)
