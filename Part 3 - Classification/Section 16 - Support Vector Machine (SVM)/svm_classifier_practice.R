2f8432a500366bb355a65852a62fa14471c6cf8c
Shubham Roy <36968449+ShubhamRoy96@users.noreply.github.com>
2020-05-09 23:54:10 +0530

    Practices svm classifier

---
+++ Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm_classifier_practice.R
@@ -0,0 +1,48 @@
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[, 3:5]
+library(e1071)
+library(caTools)
+library(ElemStatLearn)
+dataset[, -3] = scale(dataset[, -3])
+split = sample.split(dataset, SplitRatio = 0.75)
+training_set = subset(dataset , subset = split == TRUE)
+test_set = subset(dataset , subset = split == FALSE)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set, 
+                 type = 'C-classification',
+                 kernel = 'linear')
+y_pred = predict(classifier,newdata = test_set[, -3])
+
+cm = table(test_set[, 3], y_pred)
+
+set = training_set
+X1 = seq(from = min(set[,1]) - 1, to = max(set[, 1]) + 1, by = 0.01)
+X2 = seq(from = min(set[,2]) - 1, to = max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1,X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier,newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training Set)',
+     xlab = 'Age',
+     ylab = 'Estimated Salary',
+     xlim = range(X1),
+     ylim = range(X2))
+contour(X1,X2,matrix(as.numeric(y_grid), length(X1), length(X2)),  add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1 , 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4','red3'))
+
+set = test_set
+X1 = seq(from = min(set[,1]) - 1, to = max(set[, 1]) + 1, by = 0.01)
+X2 = seq(from = min(set[,2]) - 1, to = max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1,X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier,newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Test Set)',
+     xlab = 'Age',
+     ylab = 'Estimated Salary',
+     xlim = range(X1),
+     ylim = range(X2))
+contour(X1,X2,matrix(as.numeric(y_grid), length(X1), length(X2)),  add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1 , 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4','red3'))
